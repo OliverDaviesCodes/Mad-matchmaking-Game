233241e3e3a3a98f55d68be375e57c955a46053d
OliverDaviesCodes <oliverdavieswork@gmail.com>
2021-08-12 09:09:42 +0000

    Created Javascript function for Cards and created function for flips and and accurate timer countdown during the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ class AudioController {
     constructor(){
         // created background music
         this.bgMusic = new Audio('assets/Audio/jungle.mp3');
-        this.bgMusic.volume = 0.2;
+        this.bgMusic.volume = 0.05;
         // This is what loops the background music
         this.bgMusic.loop = true;
     }
@@ -16,16 +16,121 @@ class AudioController {
     }
 }
 
-function ready() {
+class matchmaking {
+    constructor(totalTime, cards){
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById('time-remaining');
+        this.ticker = document.getElementById('flips');
+        this.audioController = new AudioController();
+    }
+    startGame() {
+        this.cardToCheck = null;
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.matchedCards = [];
+        this.busy = true;
 
-    console.log("FUNCTION READY IS TRIGGERED")
+        setTimeout(()=> {
+            this.audioController.startMusic();
+            this.shuffleCards();
+            this.countDown = this.startCountDown();
+            this.busy = false;
+        },500);
+        this.hideCards();
+        this.timer.innerHTML = this.timeRemaining;
+        this.ticker.innerHTML = this.totalClicks;
+    }
+    hideCards(){
+        this.cardsArray.forEach(card => {
+            card.classList.add('visible');
+            card.classList.add('matched');
+        });
+    }
+// Flipping Cards
+    turnCard(card){
+        if(this.canTurnCard(card)) {
+            this.totalClicks++;
+            this.ticker.innerHTML = this.totalClicks;
+            card.classList.remove('visible');
+
+            if(this.cardToCheck)
+                this.checkForMatch(card);
+            else
+                this.cardToCheck = card;
+        }
+    }
+    checkForMatch(card){
+        if(this.cardType(card) === this.cardType(this.cardToCheck))
+            // if matched
+            this.matched(card, this.cardToCheck);
+        else
+            this.misMatch(card, this.cardToCheck);
+    
+    }
+    matched(card1, card2){
+        this.matchedCards.push(card1);
+        this.matchedCards.push(card2);
+        card1.classList.add('matched');
+        card2.classList.add('matched');
+        if(this.matchedCards.length === this.cardsArray) 
+            this.victory();
+
+    }
+    misMatched(card){
+
+    }
+
+
+    cardType(){//maybe an issue at the index value
+        return card.getElementsByClassName('animals')[0].src;
+    }
+
+    startCountDown(){
+        return setInterval(()=> {
+            this.timeRemaining--;
+            this.timer.innerHTML = this.timeRemaining;
+            if(this.timeRemaining === 0)
+            this.gameOver();
+        }, 1000);
+    }
+
+    gameOver(){
+        clearInterval(this.countDown);
+        document.getElementById('gameover').classList.add('visible');
+
+    }
+    victory(){
+        clearInterval(this.countDown);
+        document.getElementById('win').classList.add('visible');
+    }
+// shuffling cards
+    shuffleCards() {
+        for( let i = this.cardsArray.length -1 ; i > 0; i--) {
+            let randomCard = Math.floor(Math.random()*(i+1));
+            this.cardsArray[randomCard].style.order = i;
+            this.cardsArray[i].style.order = randomCard;
+        }
+    }
+
+    canTurnCard(card) {
+        return true;
+      // return (!this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck)
+    }
+}
+
+function ready() {
     
     let overlays = Array.from(document.getElementsByClassName('game-overlay'));
     let cards = Array.from(document.getElementsByClassName('card'));
-// start game overlay
+    let game = new matchmaking(75, cards);
+    // start game overlay
     overlays.forEach (overlay =>  {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
+            game.startGame();
+
 
             let audioController = new AudioController();
             audioController.startMusic();
@@ -34,8 +139,8 @@ function ready() {
     cards.forEach(card =>{
         card.addEventListener('click', () => {
             // Turning cards 
+            game.turnCard(card);
         });
     });
-    console.log('working');
 }
 ready();
