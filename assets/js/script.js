cd1ebbb45cdcbc7a7805d72b1a8525331cfbf33e
OliverDaviesCodes <oliverdavieswork@gmail.com>
2021-08-15 12:00:08 +0000

    created advanced.html and used javascript and css to implement a more diverse and larger card base for the advanced game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,102 +1,107 @@
 function popUp() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
-  }
+}
 
 class AudioController {
-    constructor(){
+    constructor() {
         // created background music
         this.bgMusic = new Audio('assets/Audio/jungle.mp3');
         this.bgMusic.volume = 0.05;
         // This is what loops the background music
         this.bgMusic.loop = true;
     }
-    startMusic(){
+    startMusic() {
         this.bgMusic.play();
     }
-    stopMusic(){
+    stopMusic() {
         this.bgMusic.pause();
         this.bgMusic.currentTime = 0;
     }
 }
 
-const cards = [
-    {
-      name: 'hippo',
-      alt: 'this is an image of a hippo'
+const cards = [{
+        name: 'hippo',
+        alt: 'this is an image of a hippo'
     },
     {
-      name: 'lion',
-      alt: 'this is an image of a lion'
+        name: 'lion',
+        alt: 'this is an image of a lion'
     },
     {
-      name: 'tortoise',
-      alt: 'this is an image of a tortoise'
+        name: 'tortoise',
+        alt: 'this is an image of a tortoise'
     },
     {
         name: 'Elephant',
         alt: 'this is an image of a elephant'
-      },
-      {
+    },
+    {
         name: 'giraffe',
         alt: 'this is an image of a giraffe'
-      },
-      {
+    },
+    {
         name: 'monkey',
         alt: 'this is an image of a monkey'
-      },
-      {
+    },
+    {
         name: 'panda',
         alt: 'this is an image of a panda'
-      },
-      {
+    },
+    {
         name: 'penguin',
         alt: 'this is an image of a penguin'
-      },
-      {
+    },
+    {
         name: 'squirrel',
         alt: 'this is an image of a squirrel'
-      },
-      {
+    },
+    {
         name: 'zebra',
         alt: 'this is an image of a zebra'
-      },
-      {
+    },
+    {
         name: 'dolphin',
         alt: 'this is an image of a dolphin'
-      },
-      {
+    },
+    {
         name: 'goat',
         alt: 'this is an image of a goat'
-      },
-  ]; // length = 12 unique cards
-  
-  // easy = 6 unique cards
-  // hard = 10 unique cards
+    },
+]; // length = 12 unique cards
+
+// easy = 6 unique cards
+// hard = 10 unique cards
 let cardsDiv = document.getElementsByClassName('card-div');
 
 let randomizer = (n) => {
 
     let cardsCopy = [...cards];
     let arr = [];
-    for(let i = 0; i < n; i++){
-        let randomCards = Math.floor (Math.random() * cardsCopy.length);
+    for (let i = 0; i < n; i++) {
+        let randomCards = Math.floor(Math.random() * cardsCopy.length);
         arr.push(cardsCopy[randomCards]);
         cardsCopy.splice(randomCards, 1)
     }
+
     return arr;
-  // randomly select n number of cards from cardsList;
+    // randomly select n number of cards from cardsList;
 };
 
-let renderCards = (difficulty) => {
-  let numCards = difficulty = 'easy' ? 6 : 10;
-  let selectedCards = randomizer(numCards);
-  let innerHtml = '';
+let renderCards = () => {
+    let difficulty = cardsDiv[0].getAttribute("data-difficulty");
+    console.log(difficulty);
+    let numCards = difficulty === "advanced" ? 10 : 6;
+    let selectedCards = randomizer(numCards);
+    let innerHtml = '';
 
-  console.log(selectedCards)
-  
-  selectedCards.map(({name, alt}) => {
-    return innerHtml += `<div class="card visible">
+    console.log(selectedCards)
+
+    selectedCards.forEach(({
+        name,
+        alt
+    }) => {
+        innerHtml += `<div class="card visible">
         <div class="backface card-face">
             <img src="assets/images/${name}.png" alt="${alt}" class="animals">
         </div>
@@ -104,20 +109,20 @@ let renderCards = (difficulty) => {
             <img src="assets/images/logo.png" alt="mad-matchmaking logo" class="logo">
         </div>
       </div>`;
-  })
+    })
 
-  innerHtml += innerHtml;
+    innerHtml += innerHtml;
 
-  cardsDiv[0].innerHTML = innerHtml
+    cardsDiv[0].innerHTML = innerHtml
 };
 
 renderCards();
- 
+
 
 
 
 class matchmaking {
-    constructor(totalTime, cards){
+    constructor(totalTime, cards) {
         this.cardsArray = cards;
         this.totalTime = totalTime;
         this.timeRemaining = totalTime;
@@ -125,65 +130,65 @@ class matchmaking {
         this.ticker = document.getElementById('flips');
         this.audioController = new AudioController();
     }
-startGame() {
+    startGame() {
         this.cardToCheck = null;
         this.totalClicks = 0;
         this.timeRemaining = this.totalTime;
         this.matchedCards = [];
         this.busy = true;
 
-setTimeout(()=> {
+        setTimeout(() => {
             this.audioController.startMusic();
             this.shuffleCards();
             this.countDown = this.startCountDown();
             this.busy = false;
-        },500);
+        }, 500);
         this.hideCards();
         this.timer.innerHTML = this.timeRemaining;
         this.ticker.innerHTML = this.totalClicks;
     }
-hideCards(){
+    hideCards() {
         this.cardsArray.forEach(card => {
             card.classList.add('visible');
             card.classList.remove('matched');
         });
     }
-// Flipping Cards
-    turnCard(card){
-        if(this.canTurnCard(card)) {
+    // Flipping Cards
+    turnCard(card) {
+        if (this.canTurnCard(card)) {
             this.totalClicks++;
             this.ticker.innerHTML = this.totalClicks;
             card.classList.remove('visible');
 
-            if(this.cardToCheck)
+            if (this.cardToCheck)
                 this.checkForMatch(card);
             else
                 this.cardToCheck = card;
         }
     }
 
-    checkForMatch(card){
-        if(this.cardType(card) === this.cardType(this.cardToCheck))
+    checkForMatch(card) {
+        if (this.cardType(card) === this.cardType(this.cardToCheck))
             // if matched
-            
+
             this.matched(card, this.cardToCheck);
         else
             this.misMatched(card, this.cardToCheck);
-    
+
         this.cardToCheck = null;
     }
 
-    matched(card1, card2){
+    matched(card1, card2) {
         this.matchedCards.push(card1);
         this.matchedCards.push(card2);
         card1.classList.add('matched');
         card2.classList.add('matched');
-        if(this.matchedCards.length === this.cardsArray.length) 
+        if (this.matchedCards.length === this.cardsArray.length)
             this.victory();
 
     }
 
-misMatched(card1, card2){
+    misMatched(card1, card2) {
         this.busy = true;
         setTimeout(() => {
             card1.classList.add('visible');
@@ -193,52 +198,52 @@ misMatched(card1, card2){
     }
 
 
-cardType(card){//maybe an issue at the index value
+    cardType(card) { //maybe an issue at the index value
         return card.getElementsByClassName('animals')[0].src;
     }
 
-startCountDown(){
-        return setInterval(()=> {
+    startCountDown() {
+        return setInterval(() => {
             this.timeRemaining--;
             this.timer.innerHTML = this.timeRemaining;
-            if(this.timeRemaining === 0)
-            this.gameOver();
+            if (this.timeRemaining === 0)
+                this.gameOver();
         }, 1000);
     }
 
- gameOver(){
+    gameOver() {
         clearInterval(this.countDown);
         document.getElementById('gameover').classList.add('visible');
 
     }
 
-victory(){
+    victory() {
         clearInterval(this.countDown);
         document.getElementById('win').classList.add('visible');
         this.hideCards();
     }
 
-// shuffling cards
- shuffleCards() {
-        for( let i = this.cardsArray.length -1 ; i > 0; i--) {
-            let randomCard = Math.floor(Math.random()*(i+1));
+    // shuffling cards
+    shuffleCards() {
+        for (let i = this.cardsArray.length - 1; i > 0; i--) {
+            let randomCard = Math.floor(Math.random() * (i + 1));
             this.cardsArray[randomCard].style.order = i;
             this.cardsArray[i].style.order = randomCard;
         }
     }
 
     canTurnCard(card) {
-      return (!this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck)
+        return (!this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck)
     }
 }
 
 function ready() {
-    
+
     let overlays = Array.from(document.getElementsByClassName('game-overlay'));
     let cards = Array.from(document.getElementsByClassName('card'));
     let game = new matchmaking(50, cards);
     // start game overlay
-    overlays.forEach (overlay =>  {
+    overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
             game.startGame();
@@ -248,13 +253,13 @@ function ready() {
             audioController.startMusic();
         });
     });
-    cards.forEach(card =>{
+    cards.forEach(card => {
         card.addEventListener('click', () => {
             // Turning cards 
             game.turnCard(card);
         });
     });
 
-   
+
 }
 ready();
